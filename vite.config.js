6dda6f0aa82298ed03fcc4f5060b9d494a26f8d9
Bluce <a1019521a@gmail.com>
2024-03-26 17:16:17 +0800

    modify vite.json

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import {resolve} from 'path';
 
 export default defineConfig({
   plugins: [solid()],
-  //base: '/solidjs_POS_system/dist/',
+  base: '/solidjs_POS_system/dist/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),

20405a48b883f49a5d688eebe83783215a5985c4
Bluce <a1019521a@gmail.com>
2024-03-27 16:35:26 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
-import { defineConfig ,loadEnv } from 'vite';
-import solid from 'vite-plugin-solid';
-import {resolve} from 'path';
+import { defineConfig, loadEnv } from "vite";
+import solid from "vite-plugin-solid";
+import vercel from "solid-start-vercel";
+
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid({ adapter: vercel({ edge: true }) })],
   //base: '/solidjs_POS_system/dist/',
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
-      'BsScss':resolve(__dirname,'node_modules/bootstrap/scss'),
-      'bsJs':resolve(__dirname,'node_modules/bootstrap/js/dist/')
-    }
+      "@": resolve(__dirname, "src"),
+      BsScss: resolve(__dirname, "node_modules/bootstrap/scss"),
+      bsJs: resolve(__dirname, "node_modules/bootstrap/js/dist/"),
+    },
   },
-  server:{
+  server: {
     port: 8081,
-    open: '/home'},
-   
-})
+    open: "/home",
+  },
+});

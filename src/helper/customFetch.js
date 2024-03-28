3cf1bef27568cb5ba0e5fa055efb50b31e07605e
Bluce <a1019521a@gmail.com>
2024-03-28 14:06:57 +0800

    deploy zeabur

--- src/helper/customFetch.js
+++ src/helper/customFetch.js
@@ -4,17 +4,16 @@
 const baseURL = import.meta.env.VITE_VERCEL_URL;
 
 async function customFetch(url, options = {}) {
- // console.log("baseURL", process.env.VITE_VERCEL_URL);
+  // console.log("baseURL", process.env.VITE_VERCEL_URL);
   const localAccessToken = localStorage.getItem("localAccessToken"); // token儲存在localStorage
   const updatedOptions = {
     ...options,
     headers: {
       ...options.headers,
       Authorization: `Bearer ${localAccessToken}`,
-
       // Authorization:  localAccessToken,
     },
-    //credentials: "include", // 確保cookie被發送
+    credentials: "include", // 確保cookie被發送
   };
   let response = await fetch(baseURL + url, updatedOptions);
   console.log("response :", response);
@@ -24,16 +23,44 @@ async function customFetch(url, options = {}) {
   if (response.status == 401) {
     console.log("not ok response :", response);
     localStorage.setItem("localAccessToken", "");
-    location.href = "/login";
-    return Promise.reject({
-      redirect: "/login",
-      msg: "重新登入 ",
-    });
+
+    const refreshResponse = await fetch(
+      `${process.env.VERCEL_URL}/api/auth/refresh`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include", // 確保cookie被發送
+      }
+    );
+    console.log("refreshResponse: ", refreshResponse);
+    console.log("refreshResponse json: ", await refreshResponse.json());
+    if (refreshResponse.ok) {
+      const { accessToken } = await refreshResponse.json();
+      // 更新localStorage中的accessToken
+      localStorage.setItem("localAccessToken", accessToken);
+      console.log("New accessToken", accessToken);
+      // 使用新的 accessToken 重試發出請求()
+      updatedOptions.headers.Authorization = `Bearer ${accessToken}`;
+      response = await fetch(url, updatedOptions); // 使用新的 accessToken 請求回應
+
+      // location.href = "/login";
+      // return Promise.reject({
+      //   redirect: "/login",
+      //   msg: "重新登入 ",
+      // });
+    } else {
+      // 更新 refreshToken refresh failed ，跳轉到 login 頁面
+      return Promise.reject({
+        redirect: "/login",
+        msg: " refreshToken refresh failed，重新登入",
+      });
+
+      return response; // 返回原始請求回應(如果有)
+    }
   }
-
-  return response; // 返回原始請求回應(如果有)
 }
-
 export default customFetch;
 
 //if (response.status == 401) {

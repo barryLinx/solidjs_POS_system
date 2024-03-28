8cdd0c97c88067a0a6b1bc34fb3f77971169e6e0
Bluce <a1019521a@gmail.com>
2024-03-28 11:53:19 +0800

    deploy zeabur

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { resolve } from "path";
 
 export default defineConfig({
   plugins: [solid({ adapter: vercel({ edge: true }) })],
-  base:"/solidjs_POS_system/dist",
+  //base:"/solidjs_POS_system/dist",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
